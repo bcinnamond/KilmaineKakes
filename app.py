d05a3f74c45afd133f8e986525e7b2da6dc441c6
bcinnamond <158174450+bcinnamond@users.noreply.github.com>
2024-04-16 12:19:42 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,130 @@
+import boto3
+import pymysql
+from flask import Flask, render_template, request, redirect, url_for
+
+
+app = Flask(__name__)
+
+
+# Database configuration
+db_host = 'kilmainekakes.crs8soowki9c.eu-north-1.rds.amazonaws.com'
+db_username = 'admin'
+db_password = 'K1lma1n3KaK3s!'
+db_name = 'kilmainekakes'
+
+# Define database connection function
+def get_db_connection():
+    return pymysql.connect(
+        host=db_host,
+        user=db_username,
+        password=db_password,
+        database=db_name
+    )
+
+
+@app.route('/')
+def homepg():
+        return render_template('index.html')
+
+@app.route('/blog')
+def blogpg():
+    try:
+        connection = get_db_connection()
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT img_link, img_description, title, DATE_FORMAT(date, '%d/%m/%Y') AS date, content FROM blog")
+            blog_data = cursor.fetchall()
+    except pymysql.Error as e:
+        # Handle database errors
+        print(f"Error fetching blog data: {e}")
+        blog_data = []
+
+    finally:
+        connection.close()
+
+    return render_template('blog.html', blog=blog_data)
+
+@app.route('/contact')
+def contactpg():
+        return render_template('contact.html')
+
+@app.route('/gallery')
+def gallerypg():
+    connection = None
+    try:
+        connection = get_db_connection()
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT img_link, img_description FROM gallery")
+            gallery_data = cursor.fetchall()
+    except pymysql.Error as e:
+        # Handle database errors
+        print(f"Error fetching gallery data: {e}")
+        gallery_data = []
+
+    finally:
+        connection.close()
+
+    return render_template('gallery.html', gallery=gallery_data)
+
+@app.route('/login')
+def loginpg():
+        return render_template('login.html')
+
+@app.route('/owner_login', methods=['POST'])
+def owner_login():
+    USERNAME = 'kilmainekakes'
+    PASSWORD = 'K1lma1n3KaK3s!'
+
+    username = request.form['username']
+    password = request.form['password']
+    
+    # Check if username and password match the hardcoded credentials
+    if username == USERNAME and password == PASSWORD:
+        # Redirect to a success page if login is successful
+        return redirect(url_for('success'))
+    else:
+        # Redirect back to the login page with an error message if login fails
+        return redirect(url_for('login', error='Invalid username or password'))
+
+@app.route('/success')
+def success():
+    return render_template('imginfo.html')
+
+
+@app.route('/imginfo')
+def imginfo():
+        return render_template('imginfo.html')
+
+@app.route('/add_image', methods=['POST'])
+def add_image():
+    img_link = request.form['img_link']
+    img_description = request.form['img_description']
+    
+    # Insert image details into the database
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO gallery (img_link, img_description) VALUES (%s, %s)", (img_link, img_description))
+    connection.commit()
+    connection.close()
+    
+    return redirect(url_for('gallerypg'))
+
+@app.route('/add_blog', methods=['POST'])
+def add_blog():
+        img_link = request.form['img_link']
+        img_description = request.form['img_description']
+        title = request.form['title']
+        date = request.form['date']
+        content = request.form['content']
+        
+        # Insert blog details into the database
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO blog (img_link, img_description, title, date, content) VALUES (%s, %s, %s, %s, %s)", (img_link, img_description, title, date, content))
+        connection.commit()
+        connection.close()
+        
+        return redirect(url_for('blogpg'))
+
+
+if __name__ == '__main__':
+        app.run()
