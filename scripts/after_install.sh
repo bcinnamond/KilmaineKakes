219111f98c37f9b07aaea15e270f283eaf4b7b2e
bcinnamond <158174450+bcinnamond@users.noreply.github.com>
2024-04-29 22:13:14 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 echo "Logging in to Amazon ECR..."
-docker login --username AWS --password $(aws ecr get-login-password --region us-east-1) 123456789.dkr.ecr.us-east-1.amazonaws.com
+docker login --username AWS --password $(aws ecr get-login-password --region eu-north-1) 471112624607.dkr.ecr.eu-north-1.amazonaws.com/kilmainekakes
 echo "Logged in to Amazon ECR successfully."
 
 echo "Pulling image from Amazon ECR"
-docker pull 123456789.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
+docker pull 471112624607.dkr.ecr.eu-north-1.amazonaws.com/kilmainekakes:latest
 echo "Pulled image from Amazon ECR successfully."
