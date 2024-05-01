7339fa83ce0640bee798dfb105dff667f307e30d
bcinnamond <158174450+bcinnamond@users.noreply.github.com>
2024-05-01 09:24:12 +0100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,5 @@
 echo "Running container..."
-docker login --username AWS --password $(aws ecr get-login-password --region eu-north-1) 471112624607.dkr.ecr.eu-north-1.amazonaws.com/kilmainekakes
+docker login --username AWS --password-stdin $(aws ecr get-login-password --region eu-north-1) 471112624607.dkr.ecr.eu-north-1.amazonaws.com/kilmainekakes
+
+
 sudo docker run --name kilmainekakes -d -p 5000:5000 471112624607.dkr.ecr.eu-north-1.amazonaws.com/kilmainekakes:latest
