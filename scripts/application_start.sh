7320fffb258413eaa9fa2f912bb09c9ccc186485
bcinnamond <158174450+bcinnamond@users.noreply.github.com>
2024-05-01 09:54:34 +0100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,2 @@
 echo "Running container..."
-docker login -u AWS -p $(aws ecr get-login-password --region eu-north-1) 471112624607.dkr.ecr.eu-north-1.amazonaws.com/kilmainekakes
-sudo docker run --name kilmainekakes -d -p 5000:5000 471112624607.dkr.ecr.eu-north-1.amazonaws.com/kilmainekakes:latest
+docker run --name flask_app -d -p 5000:5000 471112624607.dkr.ecr.eu-north-1.amazonaws.com/kilmainekakes:latest
