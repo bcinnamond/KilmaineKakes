5902edc6666b25f76f89609f003438836bfb2de6
bcinnamond <beccacinnamond@gmail.com>
2024-05-05 15:12:22 +0100

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,7 +3,3 @@
 def test_login(app, client):
     res = client.get('/login')
     assert res.status_code == 200
-
-def test_owner_login(app, client):
-    res = client.post('/owner_login', data={'username': 'kilmainekakes', 'password': 'K1lma1n3KaK3s!'})
-    assert res.status_code == 302  # Assuming you're redirecting upon successful login
